b1aba9039906d6ddd3e73ce73f034a21f7e35557
Muhamad Luqman bin Shamsuddin <luqsam1@gmail.com>
2017-11-20 18:16:20 +0800

    Cleaning

--- Assets/Amulet Stuff Thats Gonna Need To Reoorganise/AmuletStatSaver.cs
+++ Assets/Amulet Stuff Thats Gonna Need To Reoorganise/AmuletStatSaver.cs
@@ -2,16 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-<<<<<<< HEAD:Assets/PlayerStatSaver.cs
-public class PlayerStatSaver : MonoBehaviour {
-	
-	public static PlayerStatSaver mInstance;
-	public float HP;
+public class AmuletStatSaver : MonoBehaviour {
 
-	void Awake () 
+	public static AmuletStatSaver mInstance;
+
+	public bool combatAmuletActive;
+
+	void Awake()
 	{
 		if (mInstance == null)
 		{
+			DontDestroyOnLoad(gameObject);
 			mInstance = this;
 		}
 		else if (mInstance != this)
@@ -19,48 +20,10 @@ public class PlayerStatSaver : MonoBehaviour {
 			Destroy(gameObject);
 		}
 
-		DontDestroyOnLoad (this);
 	}
 
-	void Start ()
+	void Start()
 	{
-		HP = 100;
+		combatAmuletActive = false;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-=======
-public class AmuletStatSaver : MonoBehaviour {
-
-    public static AmuletStatSaver mInstance;
-
-    public bool combatAmuletActive;
-
-    void Awake()
-    {
-        if (mInstance == null)
-        {
-            DontDestroyOnLoad(gameObject);
-            mInstance = this;
-        }
-        else if (mInstance != this)
-        {
-            Destroy(gameObject);
-        }
-
-
-    }
-
-    void Start()
-    {
-        combatAmuletActive = false;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-    }
->>>>>>> 8a9401ff761d541b7d98dc56506e7996a2d238e5:Assets/Amulet Stuff Thats Gonna Need To Reoorganise/AmuletStatSaver.cs
 }
